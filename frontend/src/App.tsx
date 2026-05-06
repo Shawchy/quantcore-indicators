7ea55301b97ca224b0b777c129226c5e32a4eaa5
Shawcy <shawchany3721@163.com>
2026-05-06 10:11:52 +0800

    "Add a new feature"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿import { BrowserRouter, Routes, Route } from 'react-router-dom'
+﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box, ColorModeScript, Spinner, Flex } from '@chakra-ui/react'
 import { Suspense, lazy } from 'react'
 import Layout from './components/Layout'
